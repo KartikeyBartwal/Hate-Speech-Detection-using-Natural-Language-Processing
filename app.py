d8017f4fe35212d7bd8e2e730b36797b08e5b544
Kartikey Bartwal <98084657+KartikeyBartwal@users.noreply.github.com>
2023-12-25 11:42:45 +0530

    Update and rename index.py to app.py

--- app.py
+++ app.py
@@ -85,7 +85,3 @@ def predict():
         return render_template('home.html',result=res)
     else:
         return render_template("home.html")
-
-
-if __name__ == "__main__":
-    app.run(debug=True , host = "0.0.0.0")
